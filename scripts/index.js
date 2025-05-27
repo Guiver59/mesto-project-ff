5b236862c8648b15a782d66848a1eb8343cbd800
belosludcev_sp <belosludtsev90@mail.ru>
2025-05-27 17:34:43 +0500

    сделал проектную

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,30 @@
-// @todo: Темплейт карточки
 
-// @todo: DOM узлы
+// Функция создания карточки
+function prepareCard(cardData, delFunc) {
+  // Темплейт карточки
+  const cardTemplateContent = document.querySelector('#card-template').content; 
+  let newCard = cardTemplateContent.querySelector('.card').cloneNode(true);
 
-// @todo: Функция создания карточки
+  // DOM узлы
+  let newCardImage = newCard.querySelector('.card__image');
+  let newCardDeleteButton = newCard.querySelector('.card__delete-button');
+  let newCardTitle = newCard.querySelector('.card__title');
+  newCardImage.src = cardData.link;
+  newCardImage.alt = cardData.name;
+  newCardDeleteButton.addEventListener('click', (evt) => { delFunc(newCard)});    
+  newCardTitle.textContent = cardData.name;
 
-// @todo: Функция удаления карточки
+  return newCard;
+}
 
-// @todo: Вывести карточки на страницу
+// Функция удаления карточки
+function deleteCard(card) {
+  card.remove();
+}
+
+// Вывести карточки на страницу
+let cardContainer = document.querySelector('.places__list');
+initialCards.forEach((item) => {
+  let newCard = prepareCard(item, deleteCard);
+  cardContainer.append(newCard);
+});
